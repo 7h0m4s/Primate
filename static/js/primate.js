d782da6d9bff03b926e957445c8220aac9afc4ab
7h0m4s <thomas.pegoraro@uqconnect.edu.au>
2014-08-15 15:45:51 +1000

    Added primate.js for our custom code. Attempted to add copy to clipboard functionality with ZeroClipboard.js

--- static/js/primate.js
+++ static/js/primate.js
@@ -1,6 +1,45 @@
+//http://jsfiddle.net/fluidblue/6scX9/
+
+
 function loginDropDown(command) {
+//This function is called if one of the buttons in the account's dropdown menu is clicked.
+//Please tell Thomas is there is a better way of laying this code out without a million if statements.
+
+if (command=="opnUrl"){
 
 
+} else if (command=="cpyUsr") {
+
+
+
+} else if (command=="cpyPswd") {
+
+
+
+} else if (command=="cpyUrl") {
+
+
+
+} else if (command=="view") {
+
+
+
+} else if (command=="edit") {
+
+
+
+} else if (command=="move") {
+
+
+
+} else if (command=="delete") {
+
+
+
+} else {
+	alert("Error");
+
+}
 
 
 
