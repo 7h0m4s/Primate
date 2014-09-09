3d2d9f1247d441b6c10990d1d44a1e0dc75f436f
Allen <allenps1991@hotmail.com>
2014-09-09 16:10:25 +1000

    new ui design

---
+++ newUiDesign/templates/js/primate.js
@@ -0,0 +1,18 @@
+﻿var calFrameHeight = function () {
+    var headerDimensions = $('header.bg-blue').height();
+    var bordertopbottom = 4;
+    $('#content-detail').height($(window).height() - headerDimensions - bordertopbottom);
+}
+$(function () {
+    calFrameHeight();
+    setTimeout(function () { $('.example').animate({ margin: "0", opacity: '1', }, 600); $("#loader").hide(); }, 2000);
+
+    $('#main').split({ orientation: 'vertical', limit: 220, position: '20%', });
+    $('.side-content').split({ orientation: 'vertical', limit: 220, position: '40%' });
+});
+$(window).resize(function () {
+    calFrameHeight();
+}).load(function () {
+    calFrameHeight();
+});
+
