a13aac5f092d2c4749f942d4775bc98298069df7
7h0m4s <thomas.pegoraro@uqconnect.edu.au>
2014-09-25 16:54:05 +1000

    [UNTESTED] added automatic inclusion of Static and Templates folders into the Mac .app package.

--- MakeMacPackage.sh
+++ MakeMacPackage.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 #py2applet --make-setup server.py
-#rm -rf build dist
+rm -rf build dist
+rm -rf dist
 python appSetup.py py2app
+cp -r static/ dist/server.app/Contents/Resources/
+cp -r templates/ dist/server.app/Contents/Resources/
