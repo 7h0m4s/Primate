76cc0250d07b1dcf4380e0edbb82f7197b3b268c
7h0m4s <thomas.pegoraro@uqconnect.edu.au>
2014-10-01 17:36:22 +1000

    Added /config-get and /config-set as per pre-established syntax

--- server.py
+++ server.py
@@ -680,9 +680,6 @@ def assure_path_exists(path):
 
 
 
-
-
-
 """
 Function copies specified value from specified login account to clipboard.
 uuid: UUID of account that needs value copied
@@ -723,6 +720,7 @@ Function initiates configuration file.
 """
 def initConfig():
     global confParser
+    global confPath
     confParser = SafeConfigParser()
     confPath = 'config.ini'
     if not os.path.isfile(confPath):
@@ -747,6 +745,15 @@ def initConfig():
     if not confParser.has_option("passwords", "isSymbol"):
         confParser.set('passwords', 'isSymbol', '0')
 
+    saveConfig()
+    return
+
+"""
+Function saves conficuration to file.
+"""
+def saveConfig():
+    if not os.path.isfile(confPath):
+        file(confPath, 'w').close()
     cfgFile = open(confPath,'w')
     confParser.write(cfgFile)
     cfgFile.close()
@@ -763,63 +770,55 @@ def confSetToDefault():
     confParser.set('passwords', 'isUppercase', '1')
     confParser.set('passwords', 'isDigit', '1')
     confParser.set('passwords', 'isSymbol', '0')
-    
+    saveConfig()
     return
 
-##sessionTimeOut:
-## 1
-## passwrdMinLenth:
-## 1
-## isLowercase:
-## on
-## isUppercase:
-## on
-## isDigit:
-## on
-## isSymbol:
-## on
 
-## sessionTimeOut:
-## 1
-## passwrdMinLenth:
-## 1
-## isLowercase:
-## on
-## isUppercase:
-## on
-## isDigit:
-## on
-## isSymbol:
-## on
 
 @app.route("/config-get")
 def getConfig():
     try:
         data={}
 
-        data["time_to_timeout"]=confParser.getint("general",'sessionTimeOut')
-        data["password_length"]=confParser.getint("passwords",'passwrdMinLenth')
-        data["lowercase_letters"]=confParser.getint("passwords",'isLowercase')
-        data["uppercase_letters"]=confParser.getint("passwords",'isUppercase')
-        data["digits"]=confParser.getint("passwords",'isDigit')
-        data["symbols"]=confParser.getint("passwords",'isSymbol')
         
+        data["sessionTimeOut"]=confParser.getint("general",'sessionTimeOut')
+        data["passwrdMinLenth"]=confParser.getint("passwords",'passwrdMinLenth')
+        getConfCheckboxes(data,"passwords","isLowercase")
+        getConfCheckboxes(data,"passwords","isUppercase")
+        getConfCheckboxes(data,"passwords","isDigit")
+        getConfCheckboxes(data,"passwords","isSymbol")
+      
 
         return json.dumps(data)
     except Exception,e:
             return str(e),500
+
+"""
+Helper fuction for getConfig() to help with checkbox wierdness.
+"""
+def getConfCheckboxes(dataDict, cfgSection, cfgOption):
+    if confParser.getint(cfgSection,cfgOption)==1:
+        dataDict[cfgOption]= "on"
         
+    return
 
 @app.route("/config-set",methods=['POST'])
 def setConfig():
     try:#request.form.get('test1', default=False, type=bool)
-        confParser.set("general",'time_to_timeout',request.form.get('sessionTimeOut'))
-        confParser.set("passwords",'password_length',request.form.get('passwrdMinLenth'))
+        if (request.form.get('sessionTimeOut', False) != False) and int(request.form.get('sessionTimeOut'))>0:
+            confParser.set("general",'time_to_timeout',request.values.get('sessionTimeOut'))
+        else:
+            return "No sessionTimeOut set",500
+        if request.form.get('passwrdMinLenth', False) and int(request.form.get('passwrdMinLenth'))>=0:
+            confParser.set("passwords",'password_length',request.values.get('passwrdMinLenth'))
+        else:
+            return "No password_length set",500
         setCheckBoxConfig(request,'isLowercase',"passwords")
         setCheckBoxConfig(request,'isUppercase',"passwords")
         setCheckBoxConfig(request,'isDigit',"passwords")
         setCheckBoxConfig(request,'isSymbol',"passwords")
 
+        saveConfig()
         return
     except Exception,e:
             return str(e),500
@@ -830,7 +829,7 @@ def setConfig():
 Function tests if a checkbox value exists or not and sets the approperiate value to disk.
 """
 def setCheckBoxConfig(request,cfgOption,cfgSection):
-    if request.form.get(cfgOption,False):
+    if request.values.get(cfgOption,False):
         confParser.set(cfgSection,cfgOption,1)
     else:
         confParser.set(cfgSection,cfgOption,0)
@@ -940,6 +939,10 @@ def shutdown_server():
 
 #Code below is equivilent to a "Main" function in Java or C
 if __name__ == "__main__":
+
+    #initiate server config
+    initConfig()
+    
     #initiate object that will store the databases.
     global sessionVault
     sessionVault = SessionVault()
